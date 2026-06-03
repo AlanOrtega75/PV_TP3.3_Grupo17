c6df16ef22824a3d30c594585325b91a7b4acd84
Emanuel-J-Valeriano <43277360@fi.unju.edu.ar>
2026-06-03 16:33:04 -0300

    Aporte de Emanuel copiado directamente a develop

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="app-footer">
+      <p>Proyecto TP3 - Grupo 17 · React + Vite · {new Date().getFullYear()}</p>
+    </footer>
+  );
+}
+
+export default Footer;
